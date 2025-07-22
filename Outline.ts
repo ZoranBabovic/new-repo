445a84a6f89a9b4070b27366bb22b466a5960a06
Zoran Babovic <134398323+ZoranBabovic@users.noreply.github.com>
2025-07-22 22:16:06 +0200

    Update Outline.ts

--- Outline.ts
+++ Outline.ts
@@ -1,21 +1,15 @@
-import React from 'react';
-import { Image, Link, Text, ImageField, LinkField, TextField } from '@sitecore-jss/sitecore-jss-react';
-
-export type OutlineProps = {
-  fields: {
-    image: ImageField;
-    callout: LinkField;
-    title: TextField;
-  };
-};
-
-const Outline: React.FC<OutlineProps> = ({ fields }) => (
-  <div className="outline-component">
-    {fields.title && <Text field={fields.title} tag="h2" />}
-    {fields.image && <Image field={fields.image} />}
-    {fields.callout && <Link field={fields.callout} />}
-  </div>
-);
-
-export default Outline;
-//test comment 1
+The content author can edit the following fields on the callout component:
+Image
+Optional
+Title (h2)
+Required
+Description
+Optional 
+Button 
+Optional
+Button Label
+Button URL
+Background Color Dropdown
+Options
+Primary (gray background)
+Secondary (dark background)
