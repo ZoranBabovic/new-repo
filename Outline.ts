f23ecacda5e965edc29b7684dfd951b24c471306
Zoran Babovic <134398323+ZoranBabovic@users.noreply.github.com>
2025-07-22 23:23:30 +0200

    Update Outline.ts

--- Outline.ts
+++ Outline.ts
@@ -1,15 +1,1 @@
-The content author can edit the following fields on the callout component:
-Image
-Optional
-Title (h2)
-Required
-Description
-Optional 
-Button 
-Optional
-Button Label
-Button URL
-Background Color Dropdown
-Options
-Primary (gray background)
-Secondary (dark background)
+
